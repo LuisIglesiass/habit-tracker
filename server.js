86ad50b173707bdb4a54e3df3c9d50c6cdf5dd55
Luis Iglesias <luis.iglesias@kuehne-nagel.com>
2025-06-04 02:45:12 +0200

    adjust server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const dbPath =
   process.env.NODE_ENV === "production"
@@ -209,6 +209,10 @@ app.get("/api/habits/incomplete/today/:user_id", (req, res) => {
     if (err) return res.status(400).json({ error: err.message });
     res.json(rows);
   });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // Start the server
